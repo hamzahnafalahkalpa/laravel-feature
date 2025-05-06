8e0a7ae94a2dddca7adaf92e42af27731853066c
Hamzah <hamzahnafalah@gmail.com>
2025-05-06 14:25:42 +0700

    update modules

--- src/Schemas/MasterFeature.php
+++ src/Schemas/MasterFeature.php
@@ -5,45 +5,12 @@ namespace Hanafalah\LaravelFeature\Schemas;
 use Hanafalah\LaravelFeature\{
     Supports\BaseLaravelFeature
 };
-use Hanafalah\LaravelSupport\Contracts\DataManagement;
+use Hanafalah\LaravelSupport\Contracts\Supports\DataManagement;
 
 class MasterFeature extends BaseLaravelFeature implements DataManagement
 {
-    public function booting(): self
-    {
-        static::$__class = $this;
-        static::$__model = $this->{$this->__entity . "Model"}();
-        return $this;
-    }
-
-    protected array $__guard   = ['id', 'parent_id', 'name'];
-    protected array $__add     = ['name'];
     protected string $__entity = 'MasterFeature';
     protected $__feature;
-
-    protected function booting(): void
-    {
-        parent::booting();
-        $this->__feature = self::$__model;
-    }
-
-    /**
-     * Add a new API access or update the existing one if found.
-     *
-     * The given attributes will be merged with the existing API access.
-     *
-     * @param array $attributes The attributes to be added to the API access.
-     *
-     * @return \Illuminate\Database\Eloquent\Model The API access model.
-     */
-    public function addOrChange(?array $attributes = []): self
-    {
-        $this->updateOrCreate($attributes);
-        $this->__feature = self::$__model;
-        $condition = $this->isRecentlyCreated() || isset($attributes['addFeatureVersion']);
-        if ($condition) $this->addFeatureVersion($attributes['addFeatureVersion'] ?? config('module-version.application.version_pattern'));
-        return $this;
-    }
 
     /**
      * Add a new feature version to the master feature.
@@ -73,14 +40,7 @@ class MasterFeature extends BaseLaravelFeature implements DataManagement
     }
 
     //GETTER SECTION
-    /**
-     * Retrieves a collection of features based on the given conditions.
-     *
-     * @param mixed|null $conditionls The conditions to filter the features. Default is null.
-     * @return \Illuminate\Database\Eloquent\Collection The collection of features.
-     */
-    public function getFeatureList($conditionls = null): \Illuminate\Database\Eloquent\Collection
-    {
+    public function getFeatureList($conditionls = null): \Illuminate\Database\Eloquent\Collection{
         return $this->MasterFeatureModel()->conditionals($conditionls)->get();
     }
     //END GETTER SECTION
